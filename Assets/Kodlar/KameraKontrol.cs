2ac154aa25f2c6073c4e1296382a1dc54882af07
emreoksuz <kartal_emre_53@hotmail.com>
2019-10-07 22:06:51 +0300

    Son commit

---
+++ Assets/Kodlar/KameraKontrol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraKontrol : MonoBehaviour {
+    public GameObject top;
+    Vector3 fark;
+	void Start () {
+
+        fark = transform.position - top.transform.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = top.transform.position + fark;
+
+
+	}
+}
---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour {
+    Rigidbody fizik;
+    public Text sayacTxt;
+    public Text OBTxt;
+    public int sayac = 0;
+    public int a ;
+
+    void Start () {
+        fizik = GetComponent<Rigidbody>();
+
+
+
+	}
+	
+	void FixedUpdate () {
+
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+        fizik.AddForce(vec*15);
+        
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Engel")
+        {
+
+            Destroy(other.gameObject);
+            sayac++;
+            sayacTxt.text = "Puan = " + sayac;
+
+            if (sayac == a)
+            {
+                OBTxt.text = "OYUN BİTTİ";
+
+            }
+        }
+        
+
+
+        
+    }
+
+
+
+}
